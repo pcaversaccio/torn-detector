7bfceddbd8698298e91798a33f2dab54146ae964
Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
2024-02-15 13:06:18 +0100

    ♻️ Use `eslint` Flat Configuration File `eslint.config.js`
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  {
+    files: ["**/*.{js,ts}"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      eslintConfigPrettier,
+    ],
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: { "@typescript-eslint/prefer-for-of": "off" },
+    languageOptions: {
+      ecmaVersion: "latest",
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+      },
+    },
+  },
+  {
+    ignores: ["node_modules/**", "out/**"],
+  },
+);
